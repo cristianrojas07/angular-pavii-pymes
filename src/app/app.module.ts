ec102e7dc7e9bfcdad66baa678c2674fa4e6223d
cristianrojas07 <cristianjesusrojas.chechu@gmail.com>
2020-06-22 15:36:24 -0300

    Agregado router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { MenuComponent } from './components/menu/menu.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { ListaArticulosComponent } from './components/lista-articulos/lista-articulos.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ MenuComponent, PrincipalComponent, ListaArticulosComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+        {path:'articulos', component: ListaArticulosComponent},
+        {path:'inicio', component:InicioComponent},
+        {path:'', redirectTo:'inicio', pathMatch: 'full'}
+    ])
+  ],
+  declarations: [ MenuComponent, PrincipalComponent, ListaArticulosComponent, InicioComponent ],
   bootstrap:    [ PrincipalComponent ]
 })
 export class AppModule { }
